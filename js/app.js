7c87ec83207b208825e9df2371225c58349537e8
Ross146 <ross146@mail.ru>
2015-09-15 00:30:57 +0300

    add collection

--- js/app.js
+++ js/app.js
@@ -4,24 +4,35 @@ var Person = Backbone.Model.extend({
         age: '100',
         job: 'cock sucker'
     }
-})
+});
+
+
+var PeopleCollection = Backbone.Collection.extend({
+    model: Person,
+});
 
 var PersonView = Backbone.View.extend({
     initialize: function() {
         this.render();
     },
-    template: _.template( $('#person-id').html() ),
+    template: '#person-id',
     render: function() {
-//        this.$el.html(this.model.get('age'))
-        this.$el.html( this.template( this.model.toJSON()) )
+        var template = _.template($(this.template).html());
+        this.$el.html( template( this.model.toJSON()) )
     },
     tagName: 'li'    
 })
-var person = new Person;
-var personView = new PersonView({model:person});
+//var person = new Person;
+//var personView = new PersonView({model:person});
+//
+//var person2 = new Person({'name':'Petro', 'age':'11'});
+//var personView2 = new PersonView({model:person2});
 
-var person2 = new Person({'name':'Petro', 'age':'11'});
-var personView2 = new PersonView({model:person2});
+var people = [{name: "Nick", age: 34, job: 'pornstar'},{name: "Pedro", age: 24, job: 'driver'},{name: "Huan", age: 74, job: 'oldman'}];
 
-$('body').append(personView.el);
-$('body').append(personView2.el);
+var peopleCollection = new PeopleCollection(people);
+
+
+
+//$('body').append(personView.el);
+//$('body').append(personView2.el);
