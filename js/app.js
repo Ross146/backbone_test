aa07460a7abf415d98336817018d81d19d03ce11
Evgeny Padalka <padalk46@gmail.com>
2015-09-15 17:10:02 +0300

    add collection and helper

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+
+var template = function(id) {
+    return _.template($('#' + id).html());
+}
+
+
+
+
 var Person = Backbone.Model.extend({
     defaults: {
         name: 'Pidor',
@@ -11,19 +19,40 @@ var PeopleCollection = Backbone.Collection.extend({
     model: Person,
 });
 
+var PeopleView = Backbone.View.extend({
+    tagName: 'ul',
+    
+    initialize: function() {
+        this.render();
+    },
+    
+    render: function() {
+        /*for(var i = 0; i<this.collection.length; i++) {
+            console.log(person);
+        }*/
+        
+        this.collection.each(function(person) {
+            var personView = new PersonView({model: person});
+//            console.log(personView);
+            this.$el.append(personView.el);
+        },this)
+    
+    }
+});
+
 var PersonView = Backbone.View.extend({
     initialize: function() {
         this.render();
     },
-    template: '#person-id',
+    template: template('person-id'),
     render: function() {
-        var template = _.template($(this.template).html());
-        this.$el.html( template( this.model.toJSON()) )
+//        var template = _.template(template('person-id'));
+        this.$el.html( this.template( this.model.toJSON()) )
     },
     tagName: 'li'    
 })
-//var person = new Person;
-//var personView = new PersonView({model:person});
+var person = new Person;
+var personView = new PersonView({model:person});
 //
 //var person2 = new Person({'name':'Petro', 'age':'11'});
 //var personView2 = new PersonView({model:person2});
@@ -32,7 +61,8 @@ var people = [{name: "Nick", age: 34, job: 'pornstar'},{name: "Pedro", age: 24, 
 
 var peopleCollection = new PeopleCollection(people);
 
+var peopleView = new PeopleView({collection: peopleCollection});
 
 
-//$('body').append(personView.el);
-//$('body').append(personView2.el);
+
+$('body').append(peopleView.el);
